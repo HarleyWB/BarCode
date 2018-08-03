e4c0b8ebf470c5c999fb7a70cf9478752f696569
HarleyWB <wqq1307890224@126.com>
2018-08-03 19:18:45 +0800

    集成QRcode barcode

--- NewProject/AppDelegate.m
+++ NewProject/AppDelegate.m
@@ -13,7 +13,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
- 
+    NSLog(@"w'q");
         
         self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
         // Override point for customization after application launch.
